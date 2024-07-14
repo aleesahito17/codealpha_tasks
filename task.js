5b54c048292f313987dd2b020673a8df358eed9d
aleesahito17 <145854965+aleesahito17@users.noreply.github.com>
2024-07-14 22:33:59 +0500

    PORTFOLIO-Webpage

---
+++ task.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const clickMeButton = document.getElementById('clickMeBtn');
+
+    clickMeButton.addEventListener('click', function () {
+        alert("Welcome to Yasir's portfolio! Here you can find all my project and skills.");
+    });
+});
